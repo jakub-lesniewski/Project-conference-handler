1f6b0f2d4659f438d913915dfe3dbeb4308ebe6d
BartoszPtaszynski <bartoszptasz@gmail.com>
2023-10-16 16:56:50 +0200

    Project initiated

---
+++ src/main/java/com/FMCSULconferencehandler/FmcsUlConferenceHandlerApplication.java
@@ -0,0 +1,13 @@
+package com.FMCSULconferencehandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FmcsUlConferenceHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FmcsUlConferenceHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/FMCSULconferencehandler/FmcsUlConferenceHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FMCSULconferencehandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmcsUlConferenceHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

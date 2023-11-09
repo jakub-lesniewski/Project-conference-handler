aa8dc612dcff24b662c3532073e181c5b4087e74
ayroh <jakubjanlesniewski@gmail.com>
2023-11-09 11:13:56 +0100

    removed uncessary actions

--- frontend/src/features/login/Login.jsx
+++ frontend/src/features/login/Login.jsx
@@ -1,18 +1,34 @@
-import { useState } from "react";
-import { Form, useNavigate } from "react-router-dom";
+import { Form } from "react-router-dom";
 import InputForm from "../../ui/InputForm";
 import Button from "../../ui/Button";
 import axios from "axios";
+import { useState } from "react";
+import { loginUser } from "./api";
 
 function Login() {
-  const [loginData, setLoginData] = useState({});
-  const navigate = useNavigate({
-    id: null,
-    name: null,
-    surname: null,
-    email_login: null,
-    affiliation: null,
+  const [userCredentials, setUserCredentials] = useState({
+    email: "",
+    password: "",
   });
+
+  const { email, password } = userCredentials;
+
+  function handleInputChange(e) {
+    const { name, value } = e.target;
+    setUserCredentials({
+      ...userCredentials,
+      [name]: value,
+    });
+  }
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log(userCredentials);
+
+    const response = await loginUser(email, password);
+    console.log(response);
+  }
 
   return (
     <div className="flex flex-col items-center gap-5">
@@ -20,27 +36,30 @@ function Login() {
         <h1 className="text-2xl font-semibold tracking-widest">login</h1>
       </div>
 
-      <Form method="POST" className="flex flex-col gap-5">
-        <InputForm type="text" placeholder="email" name="email" />
+      <form className="flex flex-col gap-5" onSubmit={handleSubmit}>
+        <InputForm
+          type="text"
+          placeholder="email"
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+        />
 
-        <InputForm type="password" placeholder="password" name="password" />
+        <InputForm
+          type="password"
+          placeholder="password"
+          value={password}
+          name="password"
+          onChange={handleInputChange}
+        />
 
-        <Button type="submit" />
-      </Form>
+        <Button onClick={handleSubmit} type="submit" />
+      </form>
     </div>
   );
 }
 
 export async function action({ request }) {
-  const formData = await request.formData();
-  const email = formData.get("email");
-  const token = formData.get("password");
-
-  const data = {
-    email_login: email,
-    password: token,
-  };
-
   // TODO make it a util function
   const config = {
     method: "post",
@@ -54,9 +73,8 @@ export async function action({ request }) {
 
   const response = await axios.request(config);
   const responseData = response.data;
-  console.log(responseData);
 
-  return null;
+  console.log(responseData);
 }
 
 export default Login;
---
+++ frontend/src/features/login/api.js
@@ -0,0 +1,23 @@
+/* eslint-disable no-useless-catch */
+import axios from "axios";
+
+const baseURL = "http://localhost:8080";
+
+const axiosInstance = axios.create({
+  baseURL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export async function loginUser(email, password) {
+  try {
+    const response = await axiosInstance.post("/login", {
+      email_login: email,
+      password,
+    });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+}

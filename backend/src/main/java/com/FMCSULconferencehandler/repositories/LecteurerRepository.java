506bde305d6fd0dc9f14c77346d7626dd426fcfe
Marek <marek.luszczyk@gmail.com>
2023-11-29 15:36:56 +0100

    test

--- backend/src/main/java/com/FMCSULconferencehandler/repositories/LecteurerRepository.java
+++ backend/src/main/java/com/FMCSULconferencehandler/repositories/LecteurerRepository.java
@@ -1,7 +1,7 @@
 package com.FMCSULconferencehandler.repositories;
 
 import com.FMCSULconferencehandler.model.Participant;
-import com.FMCSULconferencehandler.model.Attendance_Lecture;
+import com.FMCSULconferencehandler.model.Lecteurer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -9,7 +9,7 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 import java.util.UUID;
 
-public interface Attendence_LectureRepository extends JpaRepository<Attendance_Lecture, UUID> {
-    @Query("SELECT participant FROM Attendance_Lecture WHERE lecture.id = :idLecture")
+public interface LecteurerRepository extends JpaRepository<Lecteurer, UUID> {
+    @Query("SELECT participant FROM Lecteurer WHERE lecture.id = :idLecture")
     List<Participant> findByLectureId(@Param("idLecture") UUID idLecture);
 }

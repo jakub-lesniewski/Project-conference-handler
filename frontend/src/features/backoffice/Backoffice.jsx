b53cb2dce3acf12e23858d7802018d535f00bc7d
ayroh <jakubjanlesniewski@gmail.com>
2024-01-17 22:57:52 +0100

    minor refactoring

--- frontend/src/features/backoffice/Backoffice.jsx
+++ frontend/src/features/backoffice/Backoffice.jsx
@@ -1,11 +1,37 @@
+import React, { useState, useEffect } from "react";
 import AttendeesBox from "./attendee/AttendeesBox";
 import TimelineBox from "./timeline/TimelineBox";
 import { useBackofficeContext } from "./BackofficeContext";
 
 function Backoffice() {
   const { attendeesArr, timelineArr } = useBackofficeContext();
+  const [eventsArr, setEventsArr] = useState([]);
+  const [sessionsArr, setSessionsArr] = useState([]);
+  const [sessionData, setSessionData] = useState({});
 
-  console.log(attendeesArr, timelineArr);
+  useEffect(() => {
+    const tempeventsArr = [];
+    const tempSessions = [];
+
+    timelineArr.forEach((item) => {
+      if ("attendeeLimit" in item) {
+        tempSessions.push(item);
+      } else {
+        tempeventsArr.push(item);
+      }
+    });
+
+    setSessionData({
+      attendees: attendeesArr,
+      events: tempeventsArr,
+      sessions: tempSessions,
+    });
+
+    setEventsArr(tempeventsArr);
+    setSessionsArr(tempSessions);
+  }, [timelineArr, attendeesArr]);
+
+  console.log(sessionData);
 
   return (
     <main className="grid grid-cols-5 sm:grid-cols-1 lg:grid-cols-5 2xl:mx-20">
